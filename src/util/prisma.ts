85d299a9c58a129e16c0df8ddd7adc7140fe58b0
callumfairbairn <callum_fairbairn@hotmail.com>
2023-09-18 21:46:42 +0100

    Update prisma.ts

--- src/util/prisma.ts
+++ src/util/prisma.ts
@@ -1,16 +1,17 @@
 import { PrismaClient } from '@prisma/client'
 
-let prisma: PrismaClient;
-
-//check if we are running in production mode
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient()
-} else {
-//check if there is already a connection to the database
-  if (!global.prisma) {
-    global.prisma = new PrismaClient()
-  }
-  prisma = global.prisma
+const prismaClientSingleton = () => {
+  return new PrismaClient()
 }
 
-export { prisma };
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined
+}
+
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
+
+export default prisma
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
